dd8ee0ab17c85fabe9eb6bc9f51acf7b01c6b6e7
Misael Barbosa <91623505+Misael10x@users.noreply.github.com>
2024-11-07 15:47:31 -0600

    Update Fractal.java

--- Fractal.java
+++ Fractal.java
@@ -1,1 +1,18 @@
 
+package fractal;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Graphics;
+
+
+class Fractal extends JPanel {
+
+    
+    double xp1 = 200, yp1 = 50;  
+    double xp2 = 50, yp2 = 300;   
+    double xp3 = 350, yp3 = 300;  
+
+    double sin60 = Math.sin(Math.PI / 3);
+
+    int nivel_de_recursividad = 5;
